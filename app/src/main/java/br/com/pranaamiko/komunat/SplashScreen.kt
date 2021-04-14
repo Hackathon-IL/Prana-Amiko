fa6bcc41e336d3d72e2bf2612ff53f4d8bace37b
Alyne <42252701+alynensilva@users.noreply.github.com>
2021-04-14 15:03:59 -0300

    Initial commit

---
+++ app/src/main/java/br/com/pranaamiko/komunat/SplashScreen.kt
@@ -0,0 +1,23 @@
+package br.com.pranaamiko.komunat
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //abrir a login activity após 3 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            //iniciar um intent
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 5000)
+    }
+}
